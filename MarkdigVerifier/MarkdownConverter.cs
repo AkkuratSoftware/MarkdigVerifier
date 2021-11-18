939afc64b3098bc64b04ee872b67a54fa71d8ccc
LMarshall <leslie.j.marshall@gmail.com>
2021-11-18 10:58:51 -0500

    Initial Commit

---
+++ MarkdigVerifier/MarkdownConverter.cs
@@ -0,0 +1,18 @@
+﻿using Markdig;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkdigVerifier
+{
+    public static class MarkdownConverter
+    {
+        public static string Parse(string markdown)
+        {
+            var pipeline = new MarkdownPipelineBuilder()
+                .UseAdvancedExtensions()
+                .Build();
+            return Markdown.ToHtml(markdown, pipeline);
+        }
+    }
+}
---
+++ MarkdigVerifier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace MarkdigVerifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Markdig Verifier");
+
+            Console.WriteLine("Enter path to Markdown file.");
+            string inputPath = Console.ReadLine();
+            string outputPath = inputPath + ".html";
+
+            Console.WriteLine(string.Format("Reading {0}", inputPath));
+
+            string fileContent = File.ReadAllText(inputPath);
+
+            //Console.WriteLine(fileContent);
+
+            string htmlContent = MarkdownConverter.Parse(fileContent);
+
+            Console.WriteLine("Conversion Complete, writing to " + outputPath);
+
+            File.WriteAllText(outputPath, htmlContent);
+
+            Console.WriteLine("Finished, hit [enter] to quit.");
+            Console.ReadLine();
+
+        }
+    }
+}
